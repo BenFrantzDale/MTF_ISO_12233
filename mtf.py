1e6a435faa04b3edb873ac97727fda6936fe1073
Ben FrantzDale <ben@lantostechnologies.com>
2013-05-02 17:52:13 -0400

    Cleanup; add computeSlantEdgeMTF function.

--- mtf.py
+++ mtf.py
@@ -1,3 +1,5 @@
+# From: https://gist.github.com/stefanv/2051954
+
 from __future__ import division
 
 import numpy as np
@@ -6,8 +8,49 @@ from scipy import ndimage
 
 
 N = 100
-std = 3.75
+std = 5#3.75
 box = 5
+
+# See also: http://www.imatest.com/docs/sharpness/#calc
+
+def findStepEdgeSubpix(x, r=3):
+    """Find the position in x that has highest gradient."""
+    x = abs(ndimage.gaussian_filter(x, 2, order=1))
+    r = 3
+    peak = np.argmax(x)
+    p = np.polyfit(np.r_[peak-r:peak+r+1], np.log(x[peak-r:peak+r+1]), 2)
+    return -p[1]/(p[0]*2)
+    
+
+def superresEdge(edgeProfiles, n=4, returnBins = False):
+    """
+    Given a bunch of edge profiles, create an average
+    profile that is n times the size based on the edge positions.
+    """
+    edgePositions = np.array(map(findStepEdgeSubpix, edgeProfiles))
+    p = np.polyfit(range(len(edgePositions)), edgePositions, 2)
+    fitPositions = np.polyval(p, range(len(edgePositions)))
+    meanPos = floor(np.mean(fitPositions))
+    shifts = (floor(fitPositions) - meanPos)
+    bins = np.cast[int](np.modf(fitPositions)[0] * n)
+    edgeProfiles = [ndimage.shift(profile, -round(shft), cval=float('nan'), order=0) for profile, shft in zip(edgeProfiles, shifts)]
+    toAverage = [[] for i in range(n)]
+    for bin_i, profile in zip(bins, edgeProfiles):
+        toAverage[bin_i].append(profile)
+        
+    result = np.zeros(len(edgeProfiles[0]) * n)
+    
+    for i in range(n):
+        if not toAverage[i]: continue
+        x = np.mean(toAverage[i], 0)
+        #print 'mean',i, x
+        result[n-i-1::n] = x # The bins are sorted by increasing order of edge position; we want right-most edge to go in first bin.
+    # Trim ends -- shifts were marked with nan.
+    result = result[~isnan(result)]
+    if returnBins:
+        return result, edgeProfiles, toAverage
+    return result, edgeProfiles
+    
 
 
 def MTF(x, window=True):
@@ -42,25 +85,63 @@ def MTF(x, window=True):
     return Y[:len(Y) // 2]
 
 
-# Generate edge
-x = np.zeros(N)
-x[:N // 2] = 1
+if False:
+    # Generate edge
+    x = np.zeros(N)
+    x += 0.1
+    x[:N // 2] = 1
+    
+    # Pass through various filters
+    y0 = x[5:-5]
+    y1 = ndimage.gaussian_filter1d(x, 3.75)[5:-5]
+    y2 = np.convolve(x, 1/box * np.ones(box), mode='same')[5:-5]
+    
+    Y0 = MTF(y0)
+    Y1 = MTF(y1)
+    Y2 = MTF(y2)
+    
+    f, (ax0, ax1) = plt.subplots(1, 2)
+    ix = np.arange(len(Y1)) / (2 * len(Y1))
+    
+    ax0.plot(y0, '.-')
+    ax0.plot(y1, '.-')
+    ax0.plot(y2, '.-')
+    
+    
+    ax1.plot(ix, np.abs(Y0), '.-', label='Raw')
+    ax1.plot(ix, np.abs(Y1), '.-', label='Gaussian %.2f' % std)
+    ax1.plot(ix, np.abs(Y2), '.-', label='Box %d' % box)
+    ax1.legend()
+    
+    plt.show()
 
-# Pass through various filters
-y1 = ndimage.gaussian_filter1d(x, 3.75)[5:-5]
-y2 = np.convolve(x, 1/box * np.ones(box), mode='same')[5:-5]
-
-Y1 = MTF(y1)
-Y2 = MTF(y2)
-
-f, (ax0, ax1) = plt.subplots(1, 2)
-ix = np.arange(len(Y1)) / (2 * len(Y1))
-
-ax0.plot(y1)
-ax0.plot(y2)
-
-ax1.plot(ix, np.abs(Y1), label='Gaussian %.2f' % std)
-ax1.plot(ix, np.abs(Y2), label='Box %d' % box)
-ax1.legend()
-
-plt.show()
+if __name__ == '__main__':
+    x = np.zeros((100,300))
+    x[:, x.shape[1]//2:] = 1.0
+    x = ndimage.rotate(x, 5, order=1)
+	# Mess with the image here:
+    # x = ndimage.gaussian_filter(x, 1)
+    x = x[x.shape[0]//2-10:x.shape[0]//2+10, x.shape[1]//2-100:x.shape[1]//2+100]
+    from pylab import *
+    figure()
+    n=4
+    edges, im, theBins = superresEdge(x, n=n, returnBins=True)
+    subplot(2,2,1)
+    imshow(x,interpolation='nearest')
+    subplot(2,2,3)
+    imshow(im, interpolation='nearest')
+    subplot(2,2,2)
+    plot(edges,'.-')
+    subplot(2,2,4)
+    Y = MTF(edges/max(edges))
+    lpPerPix = n * np.arange(len(Y)) / (2 * len(Y))
+    plot(lpPerPix, abs(Y),'.-')
+    
+    if False:
+        figure()
+        title('comparing mean of FFT to the ISO 12233 way.')
+        lpPerPix_ = np.arange(len(MTF(x[0]))) / (2 * len(MTF(x[0])))
+        plot(lpPerPix_,np.mean([np.abs(MTF(xi)) for xi in x],0) / x.max(),'.-');ylim(0,1.0)
+        plot(lpPerPix, abs(Y),'g.-')
+        
+    show()
